ca4a7c0eb8c1ac1db055a65c944ec8de86b8954a
Sean Ruffolo <SeanRuffolo@terminal-de-shifkey.local>
2019-02-13 19:40:00 -0800

    search bar component and prototype event handler

---
+++ src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+
+  state = { term: ''}
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.term)
+  }
+
+  render(){
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field>">
+            <label>Image Search</label>
+            <input
+              value = {this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value})}
+              type="text"/>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+export default SearchBar;
